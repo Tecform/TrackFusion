8150d6ec206d96d19fdb817caef953322584be97
Tecform <observergamermc@gmail.com>
2024-09-03 22:15:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function updateFileList() {
 async function combineAudios() {
     const audioFiles = document.getElementById('audioFiles').files;
     if (audioFiles.length === 0) {
-        alert('Please, select atleast one file..');
+        alert('Please, select atleast one file');
         return;
     }
 
